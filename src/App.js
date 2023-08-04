70ea7180529ec019985eb2692256415da4744e27
nouh <nuuhnasir12@gmail.com>
2023-08-04 17:08:32 +0400

    u

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import { books } from "./data";
 import { useEffect, useState } from "react";
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import BookInfo from "./pages/BookInfo";
 import Cart from "./pages/Cart";
+import Footer from "./components/Footer";
 
 function App() {
   const [cart, setCart] = useState([]);
